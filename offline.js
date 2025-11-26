0f037f90f06cee4d5dcc62fee774b5219ce26fd9
phattaratidaa21eiei <phattaratidaa21@gmail.com>
2025-11-26 13:39:41 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1764132698,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player2-sheet0.png",
+		"images/obj_floor-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/golditem-sheet0.png",
+		"images/greenitem-sheet0.png",
+		"images/obj_turret-sheet0.png",
+		"images/obj_bullet-sheet0.png",
+		"images/obj_bg1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
